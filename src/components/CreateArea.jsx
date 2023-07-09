7ad3b0658aa3e5b1ea2fe39e4603de88c36b5dab
Rubesh K R <rupesh.maxpain@gmail.com>
2023-07-10 00:36:53 +0530

    Add Animation function

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+  const [flag, setFlag] = useState(false);
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  function handleClick() {
+    setFlag(true);
+  }
 
   function inputNote(event) {
     const { name, value } = event.target;
@@ -27,22 +33,27 @@ function CreateArea(props) {
   return (
     <div>
       <form className="create-note" onSubmit={addNote}>
-        <input
-          name="title"
-          onChange={inputNote}
-          placeholder="Title"
-          value={note.title}
-        />
+        {flag && (
+          <input
+            name="title"
+            onChange={inputNote}
+            placeholder="Title"
+            value={note.title}
+          />
+        )}
         <textarea
           name="content"
           onChange={inputNote}
           placeholder="Take a note..."
-          rows="3"
+          rows={flag ? "3" : "1"}
           value={note.content}
+          onClick={handleClick}
         />
-        <Fab type="submit">
-          <AddIcon />
-        </Fab>
+        <Zoom in={flag}>
+          <Fab type="submit">
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
